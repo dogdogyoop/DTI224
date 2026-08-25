d642a9530eeceb7a80801f0baa23971feae2e3b7
dogdogyoop <dogdogyoop@gmail.com>
2026-08-25 19:52:06 +0700

    Move back link outside assignment cards' git push
    clear
    git clear
    Clear-Host

--- pwa.js
+++ pwa.js
@@ -1,4 +1,10 @@
-if ("serviceWorker" in navigator) {
+const isLocalDevelopment = ["localhost", "127.0.0.1"].includes(window.location.hostname);
+
+if ("serviceWorker" in navigator && isLocalDevelopment) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+        registrations.forEach((registration) => registration.unregister());
+    });
+} else if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
         navigator.serviceWorker.register("service-worker.js").catch((error) => {
             console.error("Service worker registration failed:", error);
